d36c5bd1251387b0c35965c8cc0f3a21e553d02b
Thányla Sales <thanylas@gmail.com>
2020-02-27 16:55:13 -0300

    Alterando a imagem e layout

--- app/src/main/java/com/example/apprickandmorty/curiosidade/CuriosidadeActivity.java
+++ app/src/main/java/com/example/apprickandmorty/curiosidade/CuriosidadeActivity.java
@@ -3,6 +3,7 @@ package com.example.apprickandmorty.curiosidade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.apprickandmorty.R;
 
@@ -12,5 +13,17 @@ public class CuriosidadeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curiosidade);
+
+        //Ativar opção(seta) para retornar a activity anterior
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
